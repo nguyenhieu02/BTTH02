9a95d4bd6fb0a90437458c07f083744c71dbdc7b
GLNihilis <Nihilis9999@gmail.com>
2022-02-27 22:26:17 +0700

    TC

---
+++ Trang Chủ/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slideBlog').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  }) 
+  $(document).ready(function(){
+    $('.slide_show').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrow: false,
+    });
+  })
